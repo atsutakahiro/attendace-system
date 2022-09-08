34dd3390646d96dbeb9c9be0cc1756037eb919c0
Takahiro Mizuno <takahiro619z@icloud.com>
2022-09-08 08:55:02 +0000

    7章終了

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,6 +4,18 @@ module SessionsHelper
     session[:user_id] = user.id
   end
   
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+    
+  def forget(user)
+    user.forget
+    cookies.delete(:user_id)
+    cookies.delete(:remember_token)
+  end
+  
   def log_out
     session.delete(:user_id)
     @current_user = nil
@@ -12,10 +24,17 @@ module SessionsHelper
   
   # 現在ログイン中のユーザーがいる場合オブジェクトを返します。
   def current_user
-    if session[:user_id]
-      @current_user ||= User.find_by(id: session[:user_id])
+    if (user_id = session[:user_id])
+      @current_user ||= User.find_by(id: user_id)
+    elsif (user_id = cookies.signed[:user_id])
+      user = User.find_by(id: user_id)
+      if user && user.authenticated?(cookies[:remember_token])
+        log_in user
+        @current_user = user
+      end
     end
   end
+
   
   # 現在ログイン中のユーザーがいればtrue、そうでなければfalseを返します。
   def logged_in?
