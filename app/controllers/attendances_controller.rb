bc54115c52c0e4757e29d05798872aebd3b398db
Takahiro Mizuno <takahiro619z@icloud.com>
2022-09-24 19:33:52 +0000

    aa

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,6 +1,7 @@
 class AttendancesController < ApplicationController
   before_action :set_user, only: [:edit_one_month, :update_one_month]
   before_action :logged_in_user, only: [:update, :edit_one_month]
+  before_action :admin_or_correct_user, only: [:update, :edit_one_month, :update_one_month]
   before_action :set_one_month, only: :edit_one_month
 
   UPDATE_ERROR_MSG = "勤怠登録に失敗しました。やり直してください。"
@@ -9,7 +10,7 @@ class AttendancesController < ApplicationController
     @user = User.find(params[:user_id])
     @attendance = Attendance.find(params[:id])
     # 出勤時間が未登録であることを判定します。
-    if @attendance.started_at.nil?
+    if @attendance.started_at.nil? 
       if @attendance.update_attributes(started_at: Time.current.change(sec: 0))
         flash[:info] = "おはようございます！"
       else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,55 +1,52 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :logged_in_user, only: [:index, :edit, :update, :destroy, :edit_basic_info, :update_basic_info]
-  before_action :correct_user, only: [:edit, :update]
-  before_action :admin_user, only: [:destroy, :edit_basic_info, :update_basic_info]
-  before_action :set_one_month, only: :show
-
+   before_action :set_user, only: [:show, :edit, :update, :edit_basic_info, :update_basic_info]
+   before_action :logged_in_user, only: [:index, :show, :edit, :update, :edit_basic_info, :update_basic_info]
+   before_action :correct_user, only: [:edit, :update]
+   before_action :admin_user, only: [:edit_basic_info, :update_basic_info]
+   before_action :set_one_month, only: :show
+   
   def index
-    @users = User.paginate(page: params[:page])
+    if params[:search].present?
+      @users = @users.where('name LIKE ?', "%#{params[:search]}%")
+    else
+      @users = User.paginate(page: params[:page])
+    end
   end
-
+  
   def show
     @worked_sum = @attendances.where.not(started_at: nil).count
   end
-
+ 
   def new
     @user = User.new
   end
-
+  
   def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
       flash[:success] = '新規作成に成功しました。'
-      redirect_to @user
+      redirect_to @user 
     else
       render :new
     end
   end
-
+  
   def edit
   end
-
+  
   def update
     if @user.update_attributes(user_params)
-      flash[:success] = "ユーザー情報を更新しました。"
+      flash[:success] = "アカウント情報を更新しました。"
       redirect_to @user
     else
-      render :edit      
+      render :edit
     end
   end
-
-  def destroy
-    @user.destroy
-    flash[:success] = "#{@user.name}のデータを削除しました。"
-    redirect_to users_url
-  end
-
+  
   def edit_basic_info
-    @user = User.find(params[:id])
   end
-
+  
   def update_basic_info
     if @user.update_attributes(basic_info_params)
       flash[:success] = "#{@user.name}の基本情報を更新しました。"
@@ -58,13 +55,13 @@ class UsersController < ApplicationController
     end
     redirect_to users_url
   end
-
+  
   private
 
     def user_params
       params.require(:user).permit(:name, :email, :department, :password, :password_confirmation)
     end
-
+    
     def basic_info_params
       params.require(:user).permit(:department, :basic_time, :work_time)
     end
