93f7e07dffbbb86bd0a729313b2e1e4a8c3067ff
EC2 Default User <ec2-user@ip-172-31-45-77.ap-northeast-1.compute.internal>
2022-10-19 01:40:10 +0000

    残業申請モーダル上部完成

--- config/routes.rb
+++ config/routes.rb
@@ -15,11 +15,11 @@ Rails.application.routes.draw do
       patch 'attendances/update_one_month'
     end
   resources :attendances, only: [:update] do
-    member do
-      # 残業申請モーダル
-      get 'edit_overtime_request'
-      patch 'update_overtime_request'
-    end
-    end
+      member do
+        # 残業申請モーダル
+        get 'edit_overtime_request'
+        patch 'update_overtime_request'  
+      end
+  end
   end
 end
