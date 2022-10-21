37955258b4fabc54d10fefaf7de2cdbdb0a8fa72
EC2 Default User <ec2-user@ip-172-31-45-77.ap-northeast-1.compute.internal>
2022-10-21 11:58:46 +0000

    migration修復

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20221013083619) do
+ActiveRecord::Schema.define(version: 20221021115326) do
 
   create_table "attendances", force: :cascade do |t|
     t.date "worked_on"
@@ -32,13 +32,14 @@ ActiveRecord::Schema.define(version: 20221013083619) do
     t.string "remember_digest"
     t.boolean "admin", default: false
     t.string "department"
-    t.datetime "basic_time", default: "2022-10-05 23:00:00"
-    t.datetime "work_time", default: "2022-10-05 22:30:00"
-    t.datetime "designated_work_start_time", default: "2022-10-13 00:00:00"
-    t.datetime "designated_work_end_time", default: "2022-10-13 09:00:00"
+    t.datetime "basic_time", default: "2022-10-20 23:00:00"
+    t.datetime "work_time", default: "2022-10-20 22:30:00"
+    t.datetime "designated_work_end_time", default: "2022-10-21 09:00:00"
     t.datetime "basic_work_time"
     t.string "uid"
     t.string "employee_number"
+    t.time "designated_work_start_time", default: "2000-01-01 09:00:00"
+    t.boolean "superior", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
