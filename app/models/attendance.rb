548cde40f9cd20f188f70c68bbb13a676cfb1efe
EC2 Default User <ec2-user@ip-172-31-45-77.ap-northeast-1.compute.internal>
2022-12-15 09:31:18 +0000

    validation OK

--- app/models/attendance.rb
+++ app/models/attendance.rb
@@ -6,15 +6,12 @@ class Attendance < ApplicationRecord
 
   # 出勤時間が存在しない場合、退勤時間は無効
   validate :finished_edit_at_is_invalid_without_a_started_edit_at
-  
   validate :started_edit_at_is_invalid_without_a_finished_edit_at
-  
-
   # 出勤・退勤時間どちらも存在する時、出勤時間より早い退勤時間は無効
   validate :started_edit_at_than_finished_edit_at_fast_if_invalid
 
   def finished_edit_at_is_invalid_without_a_started_edit_at
-    errors.add(:started_at, "が必要です") if (started_edit_at.present? && finished_edit_at.present?)
+    errors.add(:started_at, "が必要です") if (started_edit_at.blank? && finished_edit_at.present?)
   end
   
   def started_edit_at_is_invalid_without_a_finished_edit_at
@@ -22,11 +19,6 @@ class Attendance < ApplicationRecord
       errors.add(:finished_at, "が必要です") 
     end 
   end
-    
-  
-  
-  
-  
 
   def started_edit_at_than_finished_edit_at_fast_if_invalid
     if started_edit_at.present? && finished_edit_at.present?
