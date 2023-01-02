9f8bcc0d960a8bc289252ac11be595fef410cdb2
EC2 Default User <ec2-user@ip-172-31-45-77.ap-northeast-1.compute.internal>
2023-01-02 08:55:21 +0000

    勤怠

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,7 +22,7 @@ class UsersController < ApplicationController
   # def search
   #   @users = User.search(params[:name])
   # end
- def import
+  def import
    if params[:file].blank?
      flash[:danger]= "csvファイルを選択してください"
    else 
@@ -146,31 +146,57 @@ class UsersController < ApplicationController
 
     
     def send_attendances_csv(attendances)
-          csv_data = CSV.generate do |csv|
-            header = %w(日付 出社 退社)
-            csv << header
-            attendances.each do |day|
-              values = [
-                day.worked_on.strftime("%Y年%m月%d日(#{$days_of_the_week[day.worked_on.wday]})"),
-                if day.started_edit_at.nil? || day.indicater_select == "否認"
-                  l(day.started_at.floor_to(15.minutes), format: :time) if day.started_at.present?
-                else
-                  l(day.started_edit_at.floor_to(15.minutes), format: :time) 
-                end,
-                if day.finished_edit_at.nil? || day.indicater_select == "否認"
-                  l(day.finished_at.floor_to(15.minutes), format: :time) if day.finished_at.present?
-                else
-                  l(day.finished_edit_at.floor_to(15.minutes), format: :time) 
-                end,
-                ]
-                csv << values
-              end
-            end
-            send_data(csv_data, filename: "勤怠一覧表.csv")
+      #文字化け防止
+      bom = "\uFEFF"
+      # CSV.generateとは、対象データを自動的にCSV形式に変換してくれるCSVライブラリの一種
+      csv_data = CSV.generate do |csv|
+        # %w()は、空白で区切って配列を返します
+        header = %w(日付 出勤時間 退勤時間)
+        # csv << column_namesは表の列に入る名前を定義します。
+        csv << header
+        # column_valuesに代入するカラム値を定義します。
+      
+        attendances.each do |day|
+          column_values = [
+             l(day.worked_on, format: :short), 
+            if day.started_edit_at.present? && day.indicater_select == "承認"
+              l(day.started_edit_at.floor_to(15.minutes), format: :time) 
+            else
+              l(day.started_at.floor_to(15.minutes), format: :time) if day.started_at.present?
+            end,
+            if day.finished_edit_at.present? && day.indicater_select == "承認"
+              l(day.finished_edit_at.floor_to(15.minutes), format: :time) 
+            else
+              l(day.finished_at.floor_to(15.minutes), format: :time) if day.finished_at.present?
+            end,
+          ]
+        # csv << column_valueshは表の行に入る値を定義します。
+          csv << column_values
         end
-    
-        # 取得できるものは以下と同じ @user = User.find(params[:id])
-        # @user = User.find(params[:attendance][:user_id])
-        # @attendance = @user.attendances.find(params[:attendance][:id])
+      end
+      # csv出力のファイル名を定義します。
+      send_data(csv_data, filename: "勤怠一覧.csv")
+    end
+
+    # def send_attendances_csv(attendances)
+    #   csv_data = CSV.generate do |csv|
+    #     header = %w(日付 出社 退社)
+    #     csv << header
+    #     attendances.each do |attendance|
+    #       values = [
+    #         l(attendance.worked_on, format: :short),
+    #         if attendance.started_at.present?
+    #         l(attendance.started_at, format: :time) end ,
+    #         if attendance.finished_at.present?
+    #         l(attendance.finished_at, format: :time)  end,
+    #         ]
+    #         csv << values
+    #       end
+    #     end
+    #     send_data(csv_data, filename: "勤怠一覧表.csv")
+    # end
+
+    # 取得できるものは以下と同じ @user = User.find(params[:id])
+    # @user = User.find(params[:attendance][:user_id])
+    # @attendance = @user.attendances.find(params[:attendance][:id])
 end
-    
